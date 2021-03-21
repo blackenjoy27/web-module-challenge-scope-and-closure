d71260c3ffdcee443294840a2ea09e3299a295db
Xunxin Li <xli9400@Xunxins-MBP.lan1>
2021-03-21 18:19:21 -0400

    Finish Everything

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+      counter1 is returning a function and counter2 is returning a number.
   
   2. Which of the two uses a closure? How can you tell?
+
+      counter1 is using closure because it's returning a function from a function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+      When there is more than one thing need to be count on.
 */
 
 // counter1 code
@@ -62,8 +68,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()*3);
 }
 
 
@@ -81,8 +87,15 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(fn,num){
+  let home = 0;
+  let away = 0;
+  while(num!==0){
+    home+=fn();
+    away+=fn();
+    num--;
+  }
+  return {Home:home,Away:away};
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +103,8 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(fn) {
+  return {Home:fn(),Away:fn()};
 }
 
 
@@ -136,9 +149,26 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
+function scoreboard(fn1,fn2,num) {
+  let away = 0;
+  let home = 0;
+  let score = [];
+  for(let i= 0;i<num;i++){
+    const temp = fn1(fn2);
+    away += temp.Away;
+    home +=temp.Home;
+    const str = `Inning ${i}: Away ${away} - Home ${home}`;
+    score.push(str);
+  }
+  if(away!==home){
+    score.push(`Final Score: Away ${away} - Home ${home}`);
+  }else{
+    score.push(`This game will require extra innings: Away ${away} - Home ${home}`);
+  }
+  return score;
   /* CODE HERE */
 }
+
 
 
 
